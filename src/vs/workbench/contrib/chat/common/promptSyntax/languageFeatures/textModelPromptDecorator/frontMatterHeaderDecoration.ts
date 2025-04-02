ab5bff1e44d9d125f3970068f5b5af2a26acc0cd
Oleg Solomko <legomushroom@gmail.com>
2025-04-02 13:25:00 -0700

    add reactive decoration logic and front matter header decorator

---
+++ src/vs/workbench/contrib/chat/common/promptSyntax/languageFeatures/textModelPromptDecorator/frontMatterHeaderDecoration.ts
@@ -0,0 +1,99 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { localize } from '../../../../../../../nls.js';
+import { Color, RGBA } from '../../../../../../../base/common/color.js';
+import { MarkdownString } from '../../../../../../../base/common/htmlContent.js';
+import { IReactiveDecorationOptions, ReactiveDecorationBase } from './reactiveDecorationBase.js';
+import { contrastBorder, registerColor } from '../../../../../../../platform/theme/common/colorRegistry.js';
+import { IColorTheme, ICssStyleCollector } from '../../../../../../../platform/theme/common/themeService.js';
+import { FrontMatterHeaderToken } from '../../../../../../../editor/common/codecs/markdownExtensionsCodec/tokens/frontMatterHeaderToken.js';
+
+/**
+ * Decoration CSS class names.
+ */
+export enum DecorationClassNames {
+	/**
+	 * TODO: @legomushroom
+	 */
+	frontMatterHeader = 'prompt-front-matter-header',
+	frontMatterHeaderInlineInactive = 'prompt-front-matter-header-inline-inactive',
+	frontMatterHeaderInlineActive = 'prompt-front-matter-header-inline-active',
+}
+
+/**
+ * Options of the decorator.
+ */
+interface IOptions extends Pick<IReactiveDecorationOptions, 'cursorPosition' | 'register'> { }
+
+/**
+ * TODO: @legomushroom
+ */
+export class FrontMatterDecoration extends ReactiveDecorationBase<FrontMatterHeaderToken> {
+	constructor(
+		token: FrontMatterHeaderToken,
+		options: IOptions,
+	) {
+		super(token, {
+			...options,
+			description: 'Front Matter decoration',
+			hoverMessage: new MarkdownString('Front Matter header'),
+			isWholeLine: true,
+			cssClassNames: {
+				active: {
+					inline: DecorationClassNames.frontMatterHeaderInlineActive,
+					normal: DecorationClassNames.frontMatterHeader,
+				},
+				inactive: {
+					inline: DecorationClassNames.frontMatterHeaderInlineInactive,
+					normal: DecorationClassNames.frontMatterHeader,
+				},
+			},
+		});
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	public static registerCssStyles(
+		theme: IColorTheme,
+		collector: ICssStyleCollector,
+	): void {
+		/**
+		 * TODO: @legomushroom
+		 */
+		const frontMatterHeaderBackgroundColor = registerColor(
+			'chat.prompt.frontMatterBackground',
+			{ dark: new Color(new RGBA(0, 0, 0, 0.20)), light: new Color(new RGBA(0, 0, 0, 0.10)), hcDark: contrastBorder, hcLight: contrastBorder, },
+			localize('chat.prompt.frontMatterBackground', "background color of a Front Matter header block."),
+		);
+
+		const styles = [];
+		styles.push(
+			`background-color: ${theme.getColor(frontMatterHeaderBackgroundColor)};`,
+		);
+
+		const frontMatterHeaderCssSelector = `.monaco-editor .${DecorationClassNames.frontMatterHeader}`;
+		collector.addRule(
+			`${frontMatterHeaderCssSelector} { ${styles.join(' ')} }`,
+		);
+
+		const inlineInactiveStyles = [];
+		inlineInactiveStyles.push('color: var(--vscode-disabledForeground);');
+
+		const inlineActiveStyles = [];
+		inlineActiveStyles.push('color: var(--vscode-foreground);');
+
+		const frontMatterHeaderInlineActiveCssSelector = `.monaco-editor .${DecorationClassNames.frontMatterHeaderInlineActive}`;
+		collector.addRule(
+			`${frontMatterHeaderInlineActiveCssSelector} { ${inlineActiveStyles.join(' ')} }`,
+		);
+
+		const frontMatterHeaderInlineInactiveCssSelector = `.monaco-editor .${DecorationClassNames.frontMatterHeaderInlineInactive}`;
+		collector.addRule(
+			`${frontMatterHeaderInlineInactiveCssSelector} { ${inlineInactiveStyles.join(' ')} }`,
+		);
+	}
+}
---
+++ src/vs/workbench/contrib/chat/common/promptSyntax/languageFeatures/textModelPromptDecorator/reactiveDecorationBase.ts
@@ -0,0 +1,180 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Position } from '../../../../../../../editor/common/core/position.js';
+import { IMarkdownString } from '../../../../../../../base/common/htmlContent.js';
+import { BaseToken } from '../../../../../../../editor/common/codecs/baseToken.js';
+import { ModelDecorationOptions } from '../../../../../../../editor/common/model/textModel.js';
+import { ObservableDisposable } from '../../../../../../../base/common/observableDisposable.js';
+import { IModelDecorationsChangeAccessor, TrackedRangeStickiness } from '../../../../../../../editor/common/model.js';
+
+/**
+ * TODO: @legomushroom
+ */
+interface ICssClassNames {
+	readonly inline: string;
+	readonly normal: string;
+}
+
+/**
+ * TODO: @legomushroom
+ */
+export interface IReactiveDecorationOptions {
+	readonly description: string;
+	readonly isWholeLine?: boolean;
+	readonly hoverMessage?: IMarkdownString | IMarkdownString[];
+	readonly cursorPosition: Position | null;
+
+	readonly register: IModelDecorationsChangeAccessor['addDecoration'];
+
+	readonly cssClassNames: {
+		readonly active: ICssClassNames;
+		readonly inactive: ICssClassNames;
+	};
+}
+
+/**
+ * TODO: @legomushroom
+ */
+export abstract class ReactiveDecorationBase<TToken extends BaseToken> extends ObservableDisposable {
+	/**
+	 * TODO: @legomushroom
+	 */
+	private currentCursorPosition: Position | null = null;
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	private readonly decorationId: string;
+
+	// /**
+	//  * TODO: @legomushroom
+	//  */
+	// private readonly onChangeEmitter = this._register(new Emitter<void>());
+
+	// /**
+	//  * TODO: @legomushroom
+	//  */
+	// private readonly onChangeEvent = this.onChangeEmitter.event;
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	private active: boolean;
+
+	constructor(
+		protected readonly token: TToken,
+		private readonly options: IReactiveDecorationOptions,
+	) {
+		super();
+
+		const { cursorPosition, register } = this.options;
+
+		this.currentCursorPosition = cursorPosition;
+		this.active = this.isActive;
+
+		this.decorationId = register(
+			this.token.range,
+			this.decorationOptions,
+		);
+
+		// TODO: @legomushroom
+		// this._register(
+		// 	this.onChange(this.updateDecoration.bind(this)),
+		// );
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	private activityStateChanged = false;
+
+	// /**
+	//  * TODO: @legomushroom
+	//  */
+	// public onChange(callback: () => void): this {
+	// 	this._register(this.onChangeEvent(callback));
+
+	// 	return this;
+	// }
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	public setCursorPosition(position: Position | null): this {
+		this.currentCursorPosition = position;
+
+		if (this.isActive !== this.active) {
+			this.active = this.isActive;
+
+			this.activityStateChanged = true;
+			// this.onChangeEmitter.fire();
+		}
+
+		return this;
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	private get isActive(): boolean {
+		if (this.currentCursorPosition === null) {
+			return false;
+		}
+
+		const { range } = this.token;
+		return range.containsPosition(this.currentCursorPosition);
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	public render(
+		accessor: IModelDecorationsChangeAccessor,
+	): this {
+		if (this.activityStateChanged === false) {
+			return this;
+		}
+
+		this.activityStateChanged = false;
+
+		accessor.changeDecorationOptions(
+			this.decorationId,
+			this.decorationOptions,
+		);
+
+		return this;
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	public remove(
+		accessor: IModelDecorationsChangeAccessor,
+	): this {
+		accessor.removeDecoration(this.decorationId);
+
+		return this;
+	}
+
+	/**
+	 * TODO: @legomushroom
+	 */
+	private get decorationOptions(): ModelDecorationOptions {
+		const classes = (this.isActive)
+			? this.options.cssClassNames.active
+			: this.options.cssClassNames.inactive;
+
+		return ModelDecorationOptions.createDynamic({
+			className: classes.normal,
+			inlineClassName: classes.inline,
+			description: this.options.description,
+			hoverMessage: this.options.hoverMessage,
+			isWholeLine: this.options.isWholeLine,
+			stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
+			shouldFillLineOnLineBreak: true,
+		});
+	}
+}
